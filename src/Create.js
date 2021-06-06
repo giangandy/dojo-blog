d3390e3be29ff61c5917c2468ca44016aaf13e1a
Andy Giang <33791995+AndyGiang123@users.noreply.github.com>
2021-06-06 17:01:03 -0400

    homepage url

--- src/Create.js
+++ src/Create.js
@@ -22,7 +22,7 @@ const Create = () => {
             console.log('new blog added');
             setIsPending(false);
 
-            history.push('/');
+            history.push('/dojo-blog');
         });
 
     }
